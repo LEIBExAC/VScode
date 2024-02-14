675951b66366c734b890415e071797894353f2ca
KuldeepBhargav <118989819+KuldeepBhargav@users.noreply.github.com>
2024-02-14 19:59:41 +0530

    Add files via upload

---
+++ UniqueLearning/login page/login.js
@@ -0,0 +1,14 @@
+var x=document.getElementById("signin");
+        var y=document.getElementById("signup");
+        var z=document.getElementById("btn");
+
+        function signup(){
+            x.style.marginLeft="-32rem";
+            y.style.marginleft="10rem";
+            z.style.left="110px";
+        }
+        function signin(){
+            x.style.marginLeft="2rem";
+            y.style.marginleft="-10rem";
+            z.style.left="0px";
+        }
