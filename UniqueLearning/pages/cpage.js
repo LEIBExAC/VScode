4409df843e5c9007393d87693126f59ec4c061ae
LEIBExAC <chaturvediaditya27@gmail.com>
2024-03-22 05:05:09 +0530

    Cousre Page Update

--- UniqueLearning/pages/cpage.js
+++ UniqueLearning/pages/cpage.js
@@ -1,4 +1,8 @@
-const courses = ['Course 1', 'Course 2', 'Course 3','course 4'];
+
+//Initially I planned to leave it as it is but, now I'm thinking to impliment it through HTML
+//We only have to change the inside content of the Course box instead of chnaging the box itself
+const courses = ['Course 1', 'Course 2', 'Course 3','course 4', 'course 5', 'course 6', 'course 7',
+ 'course 8'];
 
 const coursesContainer = document.getElementById('courses-container');
 
@@ -8,3 +12,29 @@ courses.forEach(course => {
     courseBox.textContent = course;
     coursesContainer.appendChild(courseBox);
 });
+
+
+//Sticky effect of nav bar
+window.onscroll = function() {navbarFunc()};
+//window.onscroll = function() {sidenavFunc()};
+
+var navbar = document.getElementById("navbara");
+//var sidenava = document.getElementById("sidenava");
+var sticky = navbar.offsetTop;
+//var sticky1 = sidenava.offsetLeft;
+
+/*function sidenavFunc() {
+    if (window.scrollY >= sticky1) {
+      sidenava.classList.add("sticky1")
+    } else {
+      sidenava.classList.remove("sticky1");
+    }
+  }*/
+
+function navbarFunc() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
